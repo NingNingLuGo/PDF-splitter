751af22128e613c7c05363e64efd81327125ba2e
Ernest Liu <36976375+no251-1@users.noreply.github.com>
2024-03-17 13:56:27 +0800

    stock app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,61 +1,54 @@
-import requests
-import plotly.graph_objects as go
+# 导入所需的库
+import tushare as ts
+import numpy as np
+import pandas as pd
 import streamlit as st
+import plotly.express as px
 
-# 设置API密钥
-api_id = 26672685
-app_secret = '3TCRNDEk'
+# 填入tushare的token
+pro = ts.pro_api('937bd93c07d2b4f44bdbb7982e57148fa710ba291abdd00aace96edf')
 
-# 创建一个输入框，让用户输入城市名称
-user_city = st.text_input("请输入城市名称：", "苏州")
+# 定义一个股票代码的列表
+stock_codes = ['601318.SH', '600036.SH', '600519.SH']
 
-# 当用户输入城市名称后，构造API请求URL
-url = f"https://v1.yiketianqi.com/free/week?unescape=1&appid={api_id}&appsecret={app_secret}&city={user_city}"
+# 定义一个空的列表来存储DataFrame
+data_list = []
 
-# 发送API请求并获取响应数据
-response = requests.get(url)
-data = response.json()
+# 遍历股票代码列表，通过tushare api获取股票数据
+for stock in stock_codes:
+    data = pro.daily(ts_code=stock)
+    data_list.append(data)
 
-dates = [item['date'] for item in data['data']]
-tem_day = [int(item['tem_day']) for item in data['data']]
-tem_night = [int(item['tem_night']) for item in data['data']]
+# 按行合并数据
+combined_data = pd.concat(data_list)
 
-fig = go.Figure()
-fig.add_trace(go.Scatter(x=dates, y=tem_day, mode='lines+markers', name='Day Temperature'))
-fig.add_trace(go.Scatter(x=dates, y=tem_night, mode='lines+markers', name='Night Temperature'))
+# 重置索引
+combined_data = combined_data.reset_index(drop=True)
 
-# 添加数据点的标签
-for i, date in enumerate(dates):
-    fig.add_annotation(
-        x=date,
-        y=tem_day[i],
-        text=f"{tem_day[i]}°C",
-        showarrow=False,
-        font=dict(
-            size=12,
-            color="Black"
-        ),
-        xanchor='right',
-        yanchor='bottom',
-        bgcolor="White",
-        opacity=0.8
-    )
-    fig.add_annotation(
-        x=date,
-        y=tem_night[i],
-        text=f"{tem_night[i]}°C",
-        showarrow=False,
-        font=dict(
-            size=12,
-            color="Black"
-        ),
-        xanchor='right',
-        yanchor='top',
-        bgcolor="White",
-        opacity=0.8
-    )
+# 增加'company'列
+combined_data['company'] = np.nan
 
-fig.update_layout(title='Weather Forecast', xaxis_title='Date', yaxis_title='Temperature (°C)')
+# 设置字典，提供股票代码和公司名称的映射关系
+company_dict = {'601318.SH':'中国平安','600036.SH':'招商银行','600519.SH':'茅台集团'}
 
-# 使用Streamlit的plotly_chart函数来显示图表
+# 遍历字典，将对应的公司名称填入'company'列
+for key in company_dict:
+    combined_data.loc[combined_data['ts_code']==key, 'company'] = company_dict[key]
+
+# 将'trade_date'设置为日期时间格式
+combined_data['trade_date'] = pd.to_datetime(combined_data['trade_date'])
+
+# 创建Streamlit应用
+st.title('股票收盘价图表')
+
+# 添加按钮选择log或linear
+y_axis_type = st.radio("Y轴类型:", ['log', 'linear'])
+
+# 绘制股票收盘价的图表
+fig = px.line(combined_data, x='trade_date', y='close', color='company', title='股票收盘价')
+if y_axis_type == 'log':
+    fig.update_yaxes(type='log')
+else:
+    fig.update_yaxes(type='linear')
+
 st.plotly_chart(fig)
