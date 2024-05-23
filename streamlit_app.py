4cdd26c01a923c9d7e1d6b71de4dd1264285c5fe
Ernest Liu <36976375+no251-1@users.noreply.github.com>
2024-05-23 19:42:19 +0800

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 from pyecharts.charts import Kline
 from pyecharts import options as opts
-from pyecharts.faker import Faker
 import pandas as pd
 import tushare as ts
 from pyecharts.charts import Line
+from pyecharts import options as opts
+from pyecharts.charts import Kline
+import streamlit_echarts
 
 
 # 设置tushare的token，需要先去tushare官网注册获取
@@ -75,8 +77,11 @@ def kline_chart(df):
 def main():
     st.title('K线图展示')
 
-    stock_code = st.text_input('请输入股票代码（例如：600519.SH）')
-    start_date = st.date_input('开始日期')
+    stock_code = st.text_input('请输入股票代码（例如：600519.SH）',value='600519.SH')
+
+    default_start_date = pd.Timestamp(year=2024, month=1, day=1)
+
+    start_date = st.date_input('开始日期', value=default_start_date)
     end_date = st.date_input('结束日期')
 
     data = None  # 初始化 data 变量
@@ -84,17 +89,14 @@ def main():
     if st.button('获取数据'):
         if stock_code:
             data = get_stock_data(stock_code, start_date.strftime('%Y%m%d'), end_date.strftime('%Y%m%d'))
-            #st.write(data)
             data = data.set_index('trade_date')
             data.index = pd.to_datetime(data.index)
+            data = data.sort_index()
 
 
     if data is not None:  # 只有在 data 不为 None 时才调用 kline_chart 函数
         kline = kline_chart(data)
-        kline.render("kline.html")  # 将图表保存为HTML文件
-
-        st.write("K线图示例：")
-        st.components.v1.html(open("kline.html").read(), width=800, height=600)
+        streamlit_echarts.st_pyecharts(kline,height=400)  # 直接渲染 Pyecharts 图表
 
 if __name__ == '__main__':
     main()
